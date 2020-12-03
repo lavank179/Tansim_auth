85be7c95b431ed88b8572fd7c1fb0d7f9576785c
Ch lavan kumar <chlavankumar179@gmail.com>
2020-12-03 22:41:57 +0530

    Login form UI

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Helvetica|Rubik|Lato" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="css/style.css">
     <title>PHP Login System</title>
     <!-- jQuery + Bootstrap JS -->
@@ -17,42 +18,52 @@
 
 <div class="row">
     <div class="col-sm-5 col-md-5 col-lg-5">
-        <div id="sidebar1" class="container">
-            <p id="logo">TANSIM LOGO</p>
-            <p id="p1">Entrepreneurship Development and Innovation Institute</p>
-            <p id="p2">It provides various information such as details about. Startup ecosystem in State, events.</p>
-            <p id="p3">Don’t have an account?  Sign up</p>
+        <div id="sidebar1">
+            <p id="logo"><b>TANSIM LOGO</b></p>
+            <p id="p1"><b>Entrepreneurship Development<br> and Innovation Institute</b></p>
+            <p id="p2">It provides various information such as details<br> about. Startup ecosystem in State, events.</p>
+            <p id="p3"><a href="#" style="text-decoration: none; color: #FFFFFF;">Don’t have an account?  Sign up</a></p>
         </div>
         
     </div>
     
 
     <div class="col-sm-7 col-md- col-lg-7">
-        <!-- Login script -->
-    <?php include('./controllers/login.php'); ?>
+        
+    
 
+    <div class="row">
+    <div class="col-sm-1 col-md-1 col-lg-1"></div>
+    <div class="col-sm-9 col-md-9 col-lg-9">
         <!-- Login form -->
-        <div class="App">
+        <div class="login-form">
             <div class="vertical-center">
                 <div class="inner-block">
+                <p id="p1"><b>Login</b></p>
+                <p id="p2">Hello there! Welcome Back</p>
                     <form action="" method="post">
-                        <h3>Login</h3>
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input type="email" class="form-control" name="email_signin" id="email_signin" />
+                        
+                        <div class="form-group" id="li1">
+                            <input type="email" class="form-control" name="email_signin" id="email_signin" placeholder="Enter Email"/>
                         </div>
 
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input type="password" class="form-control" name="password_signin" id="password_signin" />
+                        <div class="form-group" id="li2">
+                            <input type="password" class="form-control" name="password_signin" id="password_signin" placeholder="Enter Password" />
                         </div>
 
+                        <div>
+                            <!-- Login script -->
+                            <?php include('./controllers/login.php'); ?>
+                        </div>
                         <button type="submit" name="login" id="sign_in"
                             class="btn btn-outline-primary btn-lg btn-block">Sign
                             in</button>
                     </form>
                 </div>
             </div>
+        </div>
+        </div>
+        <div class="col-sm-2 col-md-2 col-lg-2"></div>
         </div>
     </div>
     
