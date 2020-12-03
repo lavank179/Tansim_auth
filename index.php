19f1ffba0408cbd84d7768999430168e2f6cc885
Ch lavan kumar <chlavankumar179@gmail.com>
2020-12-03 22:44:41 +0530

    Login form UI

--- index.php
+++ index.php
@@ -16,57 +16,56 @@
 <body>
 
 
-<div class="row">
-    <div class="col-sm-5 col-md-5 col-lg-5">
-        <div id="sidebar1">
-            <p id="logo"><b>TANSIM LOGO</b></p>
-            <p id="p1"><b>Entrepreneurship Development<br> and Innovation Institute</b></p>
-            <p id="p2">It provides various information such as details<br> about. Startup ecosystem in State, events.</p>
-            <p id="p3"><a href="#" style="text-decoration: none; color: #FFFFFF;">Don’t have an account?  Sign up</a></p>
-        </div>
-        
-    </div>
-    
-
-    <div class="col-sm-7 col-md- col-lg-7">
-        
-    
-
     <div class="row">
-    <div class="col-sm-1 col-md-1 col-lg-1"></div>
-    <div class="col-sm-9 col-md-9 col-lg-9">
-        <!-- Login form -->
-        <div class="login-form">
-            <div class="vertical-center">
-                <div class="inner-block">
-                <p id="p1"><b>Login</b></p>
-                <p id="p2">Hello there! Welcome Back</p>
-                    <form action="" method="post">
-                        
-                        <div class="form-group" id="li1">
-                            <input type="email" class="form-control" name="email_signin" id="email_signin" placeholder="Enter Email"/>
-                        </div>
+        <div class="col-sm-5 col-md-5 col-lg-5">
+            <div id="sidebar1">
+                <p id="logo"><b>TANSIM LOGO</b></p>
+                <p id="p1"><b>Entrepreneurship Development<br> and Innovation Institute</b></p>
+                <p id="p2">It provides various information such as details<br> about. Startup ecosystem in State, events.</p>
+                <p id="p3"><a href="#" style="text-decoration: none; color: #FFFFFF;">Don’t have an account? Sign up</a></p>
+            </div>
 
-                        <div class="form-group" id="li2">
-                            <input type="password" class="form-control" name="password_signin" id="password_signin" placeholder="Enter Password" />
-                        </div>
+        </div>
 
-                        <div>
-                            <!-- Login script -->
-                            <?php include('./controllers/login.php'); ?>
+
+        <div class="col-sm-7 col-md- col-lg-7">
+
+
+
+            <div class="row">
+                <div class="col-sm-1 col-md-1 col-lg-1"></div>
+                <div class="col-sm-9 col-md-9 col-lg-9">
+                    <!-- Login form -->
+                    <div class="login-form">
+                        <div class="vertical-center">
+                            <div class="inner-block">
+                                <p id="p1"><b>Login</b></p>
+                                <p id="p2">Hello there! Welcome Back</p>
+                                <form action="" method="post">
+
+                                    <div class="form-group" id="li1">
+                                        <input type="email" class="form-control" name="email_signin" id="email_signin" placeholder="Enter Email" />
+                                    </div>
+
+                                    <div class="form-group" id="li2">
+                                        <input type="password" class="form-control" name="password_signin" id="password_signin" placeholder="Enter Password" />
+                                    </div>
+
+                                    <div>
+                                        <!-- Login script -->
+                                        <?php include('./controllers/login.php'); ?>
+                                    </div>
+                                    <button type="submit" name="login" id="sign_in" class="btn btn-outline-primary btn-lg btn-block">Sign
+                                        in</button>
+                                </form>
+                            </div>
                         </div>
-                        <button type="submit" name="login" id="sign_in"
-                            class="btn btn-outline-primary btn-lg btn-block">Sign
-                            in</button>
-                    </form>
+                    </div>
                 </div>
+                <div class="col-sm-2 col-md-2 col-lg-2"></div>
             </div>
         </div>
-        </div>
-        <div class="col-sm-2 col-md-2 col-lg-2"></div>
-        </div>
-    </div>
-    
+
     </div>
 </body>
 
