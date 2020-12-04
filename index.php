e05ee56483fe9c7be7dc8d68335c20db3b7bd8df
Ch lavan kumar <chlavankumar179@gmail.com>
2020-12-04 15:03:38 +0530

    Toggle Sign Up - Login links

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto|Helvetica|Rubik|Lato" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="css/style.css">
-    <title>PHP Login System</title>
+    <title>TANSIM Login</title>
     <!-- jQuery + Bootstrap JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
