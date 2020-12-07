6709a834d39572c1d91085f2ff0cddba49b3edce
Ch lavan kumar <chlavankumar179@gmail.com>
2020-12-07 18:49:34 +0530

    Dashboard and other finalization

--- dashboard.php
+++ dashboard.php
@@ -20,29 +20,116 @@
         <header id="home">
             <?php include('header.php'); ?>
         </header>
+        <?php
+        if (isset($_SESSION['email'])) { ?>
 
-        <div class="container mt-5">
-            <div class="d-flex justify-content-center">
-                <div class="card" style="width: 25rem">
-                    <div class="card-body">
-                        <?php
-                        if (isset($_SESSION['email'])) { ?>
-                            <h5 class="card-title text-center mb-4">User Profile</h5>
-                            <p class="card-text">Email address: <?php echo $_SESSION['email']; ?></p>
-                            <p class="card-text">Mobile number: <?php echo $_SESSION['mobilenumber']; ?></p>
 
-                            <a class="btn btn-danger btn-block" href="logout.php">Log out</a>
-                        <?php } else {
-                            echo "<script>
+            <div class="row dash">
+                <div class="col-sm-1 col-md-1 col-lg-1"></div>
+
+                <div class="col-sm-10 col-md-10 col-lg-10">
+                    <div class="row dash">
+                        <div class="col-sm-9 col-md-9 col-lg-9">
+                            <div class="row k1">
+                                <div class="col-sm-6 col-md-6 col-lg-6">
+                                    <p class="p1"> Events List </p>
+                                </div>
+                                <div class="col-sm-6 col-md-6 col-lg-6">
+                                    <p class="p2"> View all </p>
+                                </div>
+                            </div>
+                            <hr style="border: 1px dashed #C8CFD6;" />
+
+
+                            <div class="row">
+                                <?php
+                                $email = $_SESSION['email'];
+                                $queryEvent = "SELECT * FROM events WHERE email='$email'";
+                                $result = mysqli_query($conn, $queryEvent);
+
+                                echo '';
+                                while ($row = mysqli_fetch_array($result)) {
+
+                                    $dated = $row['eventdate'];
+                                    $timed = $row['eventtime'];
+                                    $d1 = date("l", strtotime($dated));
+                                    $d2 = date("j", strtotime($dated));
+                                    $d3 = date("F, Y", strtotime($dated));
+                                    $dated = date("d F, y", strtotime($dated));
+                                    $timed = date("h:i A", strtotime($timed));
+
+                                    echo '
+                                        <div class="grid-item">
+                                            <div class="card hoverable" style="width: 18.5rem; height: 19rem;">
+                                                <div class="card-body">
+                                                    <div class="row">
+                                                                <div class="col-sm-3 col-md-3 col-lg-3 p3"> ' . $d2 . '</div>
+                                                                <div class="col-sm-9 col-md-9 col-lg-9 p4"> ' . $d1 . ' <br> <span>' . $d3 . '</span></div>
+                                                    </div>
+
+                                                    <br><br><br><br>
+                                                    <p class="p5"><b> ' . $row['title'] . '</b></p>
+                                                    <p class="p6"> ' . $row['industries'] . ' - ' . $row['sector'] . ' </p>
+                                                    <p class="p7"> Starts at ' . $timed . ' </p>
+                                                    <div class="viewbtn"><a href="http://localhost/tansim_auth/editevent.php?id=' . $row['id'] . '"><div class="p8"> <span>View / Edit</span> </div></a></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ';
+                                } ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 col-md-3 col-lg-3 sideb">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row dash">
+                                        <div class="col-sm-7 col-md-7 col-lg-7">
+                                            <p class="p1"> TANSIM Guidelines and Benefits of Organziations </p>
+                                        </div>
+                                        <div class="col-sm-5 col-md-5 col-lg-5"> <button class="btn btn-danger p2"> Get Started </button></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <p class="p3"><b> Quick Actions </b></p>
+
+                            <div class="card mt-5">
+                                <div class="card-body">
+                                    <p class="p4"> Dummy links </p>
+                                    <p class="p4"> Ecosystem </p>
+                                    <p class="p4"> Resource </p>
+                                    <p class="p4"> Blog / News </p>
+                                    <p class="p4"> Applied Events </p>
+                                    <p class="p4"> Applied Deals </p>
+                                    <p class="p4"> Applied Schemes </p>
+                                    <p class="p4"> FAQ’S </p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-1 col-md-1 col-lg-1">
+                </div>
+            </div>
+        <?php } else {
+            echo "<script>
                                     alert('A valid session was not exist. Please Login to Access.');
                                     window.location.href='http://localhost/tansim_auth';
                                   </script>";
-                        } ?>
-                    </div>
-                </div>
-            </div>
-        </div>
+        } ?>
 
+        <script>
+            $('.hoverable').mouseenter(function() {
+                $(this).addClass('shadow');
+                $(this).find(".viewbtn").show();
+            });
+            $('.hoverable').mouseleave(function() {
+                $(this).removeClass('shadow');
+                $(this).find(".viewbtn").hide();
+            });
+        </script>
     </body>
 
     </html>
--- events.php
+++ events.php
@@ -21,84 +21,76 @@
         <?php include('header.php'); ?>
     </header>
 
-    <div class="container mt-5">
-        <div class="d-flex justify-content-center">
-            <div class="card" style="width: 25rem">
-                <div class="card-body">
-                    <?php
-                    if (isset($_SESSION['email'])) { ?>
-                        <h5 class="card-title text-center mb-4">User Profile</h5>
-                        <p class="card-text">Email address: <?php echo $_SESSION['email']; ?></p>
-                        <p class="card-text">Mobile number: <?php echo $_SESSION['mobilenumber']; ?></p>
-                        <a class="btn btn-danger btn-block" href="logout.php">Log out</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row" id="pag">
-        <div class="col-sm-1 col-md-1 col-lg-1"></div>
+    <?php
+    if (isset($_SESSION['email'])) { ?>
+        <div class="row" id="pag">
+            <div class="col-sm-1 col-md-1 col-lg-1"></div>
 
-        <div class="col-sm-10 col-md-10 col-lg-10">
-            <div class="row">
-                <div class="col-sm-5 col-md-5 col-lg-5">
-                    <div class="row">
-                        <div class="col-sm-6 col-md-6 col-lg-6">
-                            <a href="http://localhost/tansim_auth/dashboard.php">
-                                <img src="https://img.icons8.com/material-sharp/24/000000/home.png" /><span id="p1"> Dashboard | Events List </span> </a>
+            <div class="col-sm-10 col-md-10 col-lg-10">
+                <div class="row">
+                    <div class="col-sm-5 col-md-5 col-lg-5">
+                        <div class="row">
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <a href="http://localhost/tansim_auth/dashboard.php">
+                                    <img src="https://img.icons8.com/material-sharp/24/000000/home.png" /><span id="p1"> Dashboard | Events List </span> </a>
+                            </div>
+                            <div class="col-sm-6 col-md-6 col-lg-6"></div>
                         </div>
-                        <div class="col-sm-6 col-md-6 col-lg-6"></div>
+                    </div>
+                    <div class="col-sm-7 col-md-7 col-lg-7"></div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+                        <p id="p2"> Events List </p>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-6" style="text-align: right; padding-top: 20px">
+                        <button class="btn btn-primary btn-md"> Add Events </button>
                     </div>
                 </div>
-                <div class="col-sm-7 col-md-7 col-lg-7"></div>
-            </div>
 
-            <div class="row">
-                <div class="col-sm-6 col-md-6 col-lg-6">
-                    <p id="p2"> Events List </p>
-                </div>
-                <div class="col-sm-6 col-md-6 col-lg-6" style="text-align: right; padding-top: 20px">
-                    <button class="btn btn-primary btn-md"> Add Events </button>
-                </div>
-            </div>
+                <div class="">
 
-            <div class="">
+                    <?php
+                    $email = $_SESSION['email'];
+                    $queryEvent = "SELECT * FROM events WHERE email='$email'";
+                    $result = mysqli_query($conn, $queryEvent);
 
-            <?php 
-                $email = $_SESSION['email'];
-                $queryEvent = "SELECT * FROM events WHERE email='$email'";
-                $result = mysqli_query($conn, $queryEvent);
+                    while ($row = mysqli_fetch_array($result)) {
 
-                while ($row = mysqli_fetch_array($result)) {
+                        $dated = $row['eventdate'];
+                        $timed = $row['eventtime'];
+                        $d1 = date("l", strtotime($dated));
+                        $dated = date("d F, y", strtotime($dated));
+                        $timed = date("h:i A", strtotime($timed));
 
-                    $dated = $row['eventdate'];
-                    $timed = $row['eventtime'];
-                    $dated = date("d F, y", strtotime($dated));
-                    $timed = date("h:i A", strtotime($timed));
-                    
-                echo '<div class="card mb-3">
+
+                        echo '<div class="card mb-3 hoverable">
                     <div class="row no-gutters">
                         <div class="col-sm-3 col-md-3 col-lg-3">
-                            <img src="./controllers/uploaded_files/'.$row['image'].'" class="card-img" alt="...">
+                            <img src="./controllers/uploaded_files/' . $row['image'] . '" class="card-img" alt="...">
                             </div>
                         <div class="col-sm-9 col-md-9 col-lg-9">
                             <div class="card-body">
                             <div class=row>
                             <div class="col-sm-8 col-md-8 col-lg-8">
-                                <p class="p3"><b> '.$row['title'].'</b></p>
-                                <p class="p4"> '.$row['short_des'].' </p>
-                                <p class="p5"> '.$row['industries'].' - '.$row['location'].' </p>
+                                <p class="p3"><b> ' . $row['title'] . '</b></p>
+                                <p class="p4"> ' . $row['short_des'] . ' </p>
+                                <p class="p5"> ' . $row['industries'] . ' - ' . $row['sector'] . ' </p>
                                 <p class="card-text p6">
                                     <div class="text-muted small">
-                                    <img src="https://img.icons8.com/color/96/000000/calendar.png"/> '. $dated .' <img src="https://img.icons8.com/material-outlined/24/000000/clock.png"/> '.$timed.' 
+                                    <img src="https://img.icons8.com/color/96/000000/calendar.png"/> ' . $dated . '&nbsp;&nbsp;&nbsp;&nbsp; 
+                                    <img src="https://img.icons8.com/material-outlined/24/000000/clock.png"/> ' . $d1 . ',  ' . $timed . ' 
                                     </div>
                                 </p>
                                 
                                 </div>
                                 <div class="col-sm-4 col-md-4 col-lg-4">
                                     <div class="lake">
-                                        <a href="http://localhost/tansim_auth/editevent.php?id='.$row['id'].'">
-                                        <span class="btn btn-md btn-primary btn-edit"><i class="bi bi-pencil-square"></i> Edit </span>
+                                        <a href="http://localhost/tansim_auth/editevent.php?id=' . $row['id'] . '">
+                                        <span class="btn btn-md btn-primary btn-edit"><img src="./assets/edit.png" /> Edit </span>
                                         </a>
+                                        <p class="p7"><img src="./assets/location.jpg" /> ' . $row['location'] . ' </p>
                                     </div>
                                 </div>
                                 </div>
@@ -106,23 +98,30 @@
                         </div>
                     </div>
                 </div>';
-                
-                } ?>
+                    } ?>
 
+                </div>
             </div>
+
+
+            <div class="col-sm-1 col-md-1 col-lg-1"></div>
         </div>
-
-
-        <div class="col-sm-1 col-md-1 col-lg-1"></div>
-    </div>
-<?php } else {
-                        echo "<script>
+    <?php } else {
+        echo "<script>
                                     alert('A valid session was not exist. Please Login to Access.');
                                     window.location.href='http://localhost/tansim_auth';
                                   </script>";
-                    } ?>
+    } ?>
 
 
+    <script>
+        $('.hoverable').mouseenter(function() {
+            $(this).addClass('shadow');
+        });
+        $('.hoverable').mouseleave(function() {
+            $(this).removeClass('shadow');
+        });
+    </script>
 </body>
 
 </html>
